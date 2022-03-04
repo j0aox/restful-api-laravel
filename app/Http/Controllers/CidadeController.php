0128d20a04f84e9b0d6775e161b10996e1ce9951
João Amador <nnettoolliveira@gmail.com>
2022-03-04 00:28:11 -0300

    adicionando model, controller e migrate de cidade

---
+++ app/Http/Controllers/CidadeController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cidade;
+use Illuminate\Http\Request;
+
+class CidadeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Cidade::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        request()->validate([
+            'name_city' => 'required'
+        ]);
+
+        return Cidade::create([
+            'name_city' => request('name_city')
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Cidade $cidade)
+    {
+        request()->validate([
+            'name_city' => 'required'
+        ]);
+    
+        $sucesso = $cidade->update([
+            'name_city' => request('name_city')
+        ]);
+    
+        return [
+            'sucess' => $sucesso
+        ];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cidade $cidade)
+    {
+        $sucesso = $cidade->delete();
+
+        return [
+            'sucess' => $sucesso
+        ];
+    }
+}
